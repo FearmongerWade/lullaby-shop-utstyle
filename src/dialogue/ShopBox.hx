b70dc8e5fdbf7471292495bdc40ee51e3216b50b
Wade <84008574+FearmongerWade@users.noreply.github.com>
2025-01-03 16:33:10 +0100

    you can select buy sell and talk now

--- src/dialogue/ShopBox.hx
+++ src/dialogue/ShopBox.hx
@@ -17,3 +17,29 @@ class ShopBox extends FlxSpriteGroup
         add(blackc);
     }
 }
+class OtherBox extends FlxSpriteGroup
+{
+    var descText:FlxText;
+
+    public function new(text:String = 'item description')
+    {
+        super();
+
+        var white = new FlxSprite(423).makeGraphic(225, 225, FlxColor.WHITE);
+        add(white);
+
+        var black = new FlxSprite(white.x+5, white.y+8).makeGraphic(206, 225, FlxColor.BLACK);
+        add(black);
+
+        descText = new FlxText(black.x + 8, black.y + 5, black.width*0.9, text, 28);
+        descText.font = Paths.font();
+        add(descText);
+
+        updateItemText();
+    }
+
+    public function updateItemText(text:String = 'new item desc')
+    {
+        descText.text = text;
+    }
+}
--- src/states/ShopState.hx
+++ src/states/ShopState.hx
@@ -1,45 +1,46 @@
 package states;
 
 import dialogue.ShopBox;
+import dialogue.ShopBox.OtherBox;
 import flixel.addons.text.FlxTypeText;
 
 class ShopState extends FlxState
 {
-    /*
-    shit to add:
-    - silver cartridge      - 0G      - Free of charge. 
-    - game boy advance sp   - 300G    - Stupid thing does nothing but make annoying sounds.
-    - mysterious letter     - 666G    - The envelope has your name on it.
-    - broken vinyl          - 250G    - I don't want this, but it's funny to make you pay for it.
-
-    sell line:
-    - Why would I buy your shit back? I'm trying to get rid of it, you moron.
-
-    clicking talk:
-    - Your nose is weird.
-    - Don't worry. I've got an eternity. Can you say the same?
-    - Don't sympathize with evil.
-    - Speaking ill of another man's hat is dishonorable.
-    - I'll have you know I don't appreciate window shoppers.
-    
-    not enough money line:
-    - You're joking, right?
-    */
     var soul:FlxSprite; // selector
 
     var curSelected:Int = 0;
     var options:Array<String> = ['Buy', 'Sell', 'Talk', 'Exit'];
     var grpText:FlxTypedGroup<FlxText>;
 
+    var prefix:FlxText;
     var dialogue:FlxTypeText;
 
     var goldText:FlxText;
     var itemsText:FlxText;
 
+    var curItem:Int = 0;
+    var itemBox:OtherBox;
+    var otherDialogue:FlxTypeText;
+    var shopItems:Array<Array<Dynamic>> = [
+        // variable - item name - price - dialogue line
+        ['Silver Cartridge',         0,   'Free of charge.'],
+        ['Game Boy Advance SP',      300, 'Stupid thing does nothing but make annoying sounds.'],
+        ['Mysterious Letter',        666, 'The envelope has your name on it.'],
+        ['Broken Vinyl',             500, "I don't want this, but it's funny to make you pay for it."],
+        ['Exit',                     0,   'Going so soon?']
+    ];
+    var grpItems:FlxTypedGroup<FlxText>;
+
+    var randomDialogue:Array<String> = [
+        "Your nose is weird.",
+        "Don't worry. I've got an eternity. Can you say the same?",
+        "Don't sympathize with evil.",
+        "Speaking ill of another man's hat is dishonorable.",
+        "I'll have you know I don't appreciate window shoppers."
+    ];
+
     override function create()
     {
-        // yeah
-
         FlxG.mouse.visible = false;
         FlxG.sound.playMusic(Paths.music('shopMenu'));
         openSubState(new substates.TransIn());
@@ -64,10 +65,18 @@ class ShopState extends FlxState
         
         // options waa
 
+        itemBox = new OtherBox();
+        add(itemBox);
+        itemBox.y = 250;
+
         var dumb = new ShopBox();
         add(dumb);
 
-        dialogue = new FlxTypeText(40, 268, 380, null, 30);
+        prefix = new FlxText(40, 268, 0, '*', 30);
+        prefix.font = Paths.font();
+        add(prefix);
+
+        dialogue = new FlxTypeText(prefix.x+25, 268, 350, null, 30);
         dialogue.font = Paths.font();
         dialogue.sounds = [FlxG.sound.load(Paths.sound('textbox/cartridgeGuy'))];
         add(dialogue);
@@ -95,16 +104,33 @@ class ShopState extends FlxState
         add(itemsText);
 
         changeItem();
-        updateText();
+        updateText(randomDialogue[FlxG.random.int(0, randomDialogue.length - 1)]);
         super.create();
     }
 
+    var selectedBuy:Bool = false;
     override function update(elapsed:Float)
     {
-        if (Controls.justPressed('menuUp'))
-            changeItem(-1);
-        if (Controls.justPressed('menuDown'))
-            changeItem(1);
+        if (!selectedBuy)
+        {
+            if (Controls.justPressed('menuUp'))
+                changeItem(-1);
+            if (Controls.justPressed('menuDown'))
+                changeItem(1);
+            if (Controls.justPressed('confirm'))
+                selectItem();
+        }
+        else
+        {
+            if (Controls.justPressed('menuUp'))
+                changeItemShop(-1);
+            if (Controls.justPressed('menuDown'))
+                changeItemShop(1);
+            if (Controls.justPressed('confirm'))
+                buyItem();
+            if (Controls.justPressed('cancel'))
+                stoppedShopping();
+        }
 
         super.update(elapsed);
     }
@@ -125,9 +151,125 @@ class ShopState extends FlxState
         FlxG.sound.play(Paths.sound('moveMenu'));
     }
 
-    function updateText(text:String = "* Don't sympathize with evil.")
+    function updateText(text:String = '')
     {
         dialogue.resetText(text);
         dialogue.start(0.04, true);
+    }
+
+    function selectItem()
+    {
+        FlxG.sound.play(Paths.sound('moveMenu'));
+
+        switch(options[curSelected])
+        {
+            case 'Buy':
+                startedShopping();
+            case 'Sell':
+                updateText("Why would I buy your shit back? I'm trying to get rid of it, you moron.");
+            case 'Talk':
+                updateText(randomDialogue[FlxG.random.int(0, randomDialogue.length - 1)]);
+            case 'Exit':
+                quitting();
+        }
+    }
+
+    function quitting()
+    {
+        updateText('Til we meet again, human.');
+        dialogue.completeCallback = function()
+        {
+            new FlxTimer().start(1, function(timer:FlxTimer) {
+                #if desktop Sys.exit(1); #end
+            });
+        }
+    }
+
+    // start of the buying sub menu
+    // this sucks i know 
+
+    function startedShopping()
+    {
+        selectedBuy = true;
+
+        // hide the previous items
+
+        prefix.alpha = 0.00001;
+        dialogue.alpha = 0.0001;
+        grpText.forEach(function(txt:FlxText) {
+            txt.alpha = 0.000001;
+        });
+
+        FlxTween.tween(itemBox, {y: 80}, 0.5, {ease:FlxEase.quadInOut});
+
+        grpItems = new FlxTypedGroup<FlxText>();
+        add(grpItems);
+
+        for (i => item in shopItems)
+        {
+            var newItem = new FlxText(50, 268+(i*35), 0, null, 28);
+            newItem.font = Paths.font();
+            newItem.ID = i;
+            grpItems.add(newItem);
+            newItem.text = item[1] + 'G - ' + item[0];
+
+            if (i == 4)
+                newItem.text = 'Exit';
+        }
+
+        otherDialogue = new FlxTypeText(450, 265, 150, "What are ya buyin'?", 28);
+        otherDialogue.font = Paths.font();
+        otherDialogue.sounds = dialogue.sounds;
+        add(otherDialogue);
+        otherDialogue.start(0.04, true);
+
+        changeItemShop();
+    }
+
+    function stoppedShopping()
+    {
+        // kill him KILL HIM. KILL HIM NOWWW
+        grpItems.forEach(function(txt:FlxText){
+            txt.kill();
+        });
+        otherDialogue.kill();
+        FlxTween.tween(itemBox, {y: 250}, 0.5, {ease:FlxEase.quadInOut});
+
+        // show the other items again
+        prefix.alpha = 1;
+        dialogue.alpha = 1;
+        grpText.forEach(function(txt:FlxText) {
+            txt.alpha = 1;
+        });
+
+        selectedBuy = false;
+        updateText(randomDialogue[FlxG.random.int(0, randomDialogue.length - 1)]);
+        changeItem();
+    }
+
+    function changeItemShop(change:Int = 0)
+    {
+        FlxG.sound.play(Paths.sound('moveMenu'));
+
+        curItem = FlxMath.wrap(curItem + change, 0, shopItems[0].length + 1);
+
+        grpItems.forEach(function(txt:FlxText)
+        {
+            if (txt.ID == curItem)
+                soul.setPosition(txt.x - 20, txt.y + 5);
+        });
+
+        itemBox.updateItemText(shopItems[curItem][2]);
+    }
+
+    function buyItem()
+    {
+        if (curItem == 4)
+            stoppedShopping();
+        else
+        {
+            FlxG.sound.play(Paths.sound('buyItem'));
+            trace('You bought ' + shopItems[curItem][0]);
+        }
     }
 }
